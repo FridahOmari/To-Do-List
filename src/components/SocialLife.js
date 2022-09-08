0f4775ca1f353b19afa7d2830d7ae70cb3b1a17a
fridah <nginaomari@gmail.com>
2022-09-08 18:54:25 +0300

    edging close on my social life component

--- src/components/SocialLife.js
+++ src/components/SocialLife.js
@@ -1,6 +1,34 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 function SocialLife() {
+    const [socialTodos, set_social_todos] = useState([])
+
+
+    useEffect(() => {
+        fetch("http://localhost:9292/people")
+        .then(res => res.json())
+        .then(data => {
+            set_social_todos(data)
+        })
+      },[])
+
+      function handleUpdateTask(updatedItem) {
+        const updatedItems = socialTodos.map((item) => {
+          if(item.id === updatedItem.id){
+            return updatedItem;
+          }
+          return item;
+        })
+        set_social_todos(updatedItems)
+      }
+    
+      
+      function handleDeletedTask(id){
+        const updatedItems = socialTodos.filter(item => item.id !== id)
+        set_social_todos(updatedItems)
+        console.log(updatedItems)
+    }
   return (
     <div>
         <div className="Container">
