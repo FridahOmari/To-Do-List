1b8e4e3cc5b71cd1acd8c4eca6f231e4189f21f8
fridah <nginaomari@gmail.com>
2022-09-08 18:42:15 +0300

    working on my navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "../App.css"
 import { Route, Switch } from "react-router-dom";
+import NavBar from "./NavBar";
 
 
 function App (){
   
   return (
     <div className="App ">
-
+        <NavBar />
     </div>
   )
 }
