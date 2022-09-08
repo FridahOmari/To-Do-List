fbd7f2f406cd427cb6074c7b464480c4127c1c85
fridah <nginaomari@gmail.com>
2022-09-08 18:21:54 +0300

    adding my route and switch from react router dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "../App.css"
-
+import { Route, Switch } from "react-router-dom";
 
 
 function App (){
   
   return (
     <div className="App ">
-
 
     </div>
   )
