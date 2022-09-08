84fed3efac31c15ebf655e9927f227a83562019f
fridah <nginaomari@gmail.com>
2022-09-08 19:00:03 +0300

    working on the all tasks component

---
+++ src/components/AllTasks.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function AllTasks() {
+  return (
+    <div className="Container">
+        <h4><em>ALL</em> Your Tasks, reminders, routines and goals will be displayed Here: </h4>
+        <ul className="Items">{todos.map(item => <Tasks 
+        key={item.id}
+        onUpdateTask={handleUpdateTask}
+        onDeleteTask={handleDeletedTask}
+        sitem={item}/>)}
+        </ul>
+    </div>
+  )
+}
+
+export default AllTasks;
