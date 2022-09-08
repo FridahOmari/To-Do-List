b289517cef204708933b29e15d2a2c59700d670e
fridah <nginaomari@gmail.com>
2022-09-08 19:03:35 +0300

    finishing up on the fetches

--- src/components/AllTasks.js
+++ src/components/AllTasks.js
@@ -3,6 +3,34 @@ import Tasks from './Tasks';
 import { useState, useEffect } from 'react';
 
 function AllTasks() {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+   
+      fetch("http://localhost:9292/all-tasks")
+  
+      .then(res => res.json())
+      .then(data => {
+        setTodos(data)
+      })
+    },[])
+
+    function handleUpdateTask(updatedItem) {
+        const updatedItems = todos.map((item) => {
+          if(item.id === updatedItem.id){
+            return updatedItem;
+          }
+          return item;
+        })
+        setTodos(updatedItems)
+    }
+    
+      
+    function handleDeletedTask(id){
+      const updatedItems = todos.filter(item => item.id !== id)
+      setTodos(updatedItems)
+      console.log(updatedItems)
+    }
   return (
     <div className="Container">
         <h4><em>ALL</em> Your Tasks, reminders, routines and goals will be displayed Here: </h4>
