e8d0a6bce990dd9029ebcb360fb73ffb98bbd48e
fridah <nginaomari@gmail.com>
2022-09-08 18:48:57 +0300

    getting my tasks component ready

---
+++ src/components/Tasks.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+function Tasks(){
+    const {task, category, isDone, id} = item
+
+    function handleDoneStatus(){
+
+        fetch(`http://localhost:9292/all-tasks/${id}`, {
+
+
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              isDone: !isDone,
+            }),
+          })
+            .then((r) => r.json())
+            .then((updatedTask) =>  onUpdateTask(updatedTask));
+    }
+
+    function handleDeleteClick(){
+
+      fetch(`http://localhost:9292/all-tasks/${id}`, {
+
+        method: "DELETE"
+      })
+      .then(res => res.json())
+      .then(() => onDeleteTask(id) )
+    }
+
+    return(
+        <div>
+            <li className={isDone ? "isDone" : ""}>
+            <span>{task}</span>
+            <span className="category">{category}</span>
+            <button className={isDone  ? "remove" : "add"}
+            onClick={handleDoneStatus}>
+                {isDone ? "Mark as Undone" : "Mark as Done"}
+            </button>
+            <button className="remove" onClick={handleDeleteClick}>Delete Task</button>
+        </li>
+        </div>
+    )
+}
+
+export default Tasks;
